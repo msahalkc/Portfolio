a357ef0d96ed64b344b0589b245dfb7077d6bc01
Muhammed Sahal K C <msahalkc@gmail.com>
2023-04-04 12:19:09 +0530

    some changes

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let activeIndex = 0;
 let nextIndex = 1;
 
-document.addEventListener('wheel', () => {
+document.addEventListener('wheel', onMouseScroll)
+
+function onMouseScroll(){
     
     nextIndex = activeIndex + 1 <= 3 ? activeIndex + 1 :0;
-
-    console.log(activeIndex, nextIndex);
 
     const currentGroup = document.querySelectorAll(`[data-index = "${activeIndex}"]`),
     nextGroup = document.querySelectorAll(`[data-index = "${nextIndex}"]`);
@@ -21,7 +21,37 @@ document.addEventListener('wheel', () => {
     activeIndex = nextIndex;
 
     nextIndex++;
+}
 
-    console.log(activeIndex, nextIndex);
+function changeSection(value) {
+    allNavs = [0,1,2,3]
 
-})
+    selectedNav = value;
+    
+    const index = allNavs.indexOf(value);
+
+    allNavs.splice(index, 1);
+
+    const hightlightNav = document.querySelectorAll(`[data-index = "${selectedNav}"]`);
+    
+    const nonHighlightedNav1 = document.querySelectorAll(`[data-index = "${allNavs[0]}"]`);
+    const nonHighlightedNav2 = document.querySelectorAll(`[data-index = "${allNavs[1]}"]`);
+    const nonHighlightedNav3 = document.querySelectorAll(`[data-index = "${allNavs[2]}"]`);
+
+    // console.log(currentGroup, nextGroup);
+    hightlightNav.forEach(element => {
+        element.dataset.status = "active";
+    });
+
+    nonHighlightedNav1.forEach(element => {
+        element.dataset.status = "inactive";
+    });
+
+    nonHighlightedNav2.forEach(element => {
+        element.dataset.status = "inactive";
+    });
+
+    nonHighlightedNav3.forEach(element => {
+        element.dataset.status = "inactive";
+    });
+}
